dc1e25469fa181f452547adf758cfa751f272898
Tulip Blossom <tulilirockz@outlook.com>
2025-11-08 03:42:02 -0300

    fix: code that zooms in a slightly better way for accessibility
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,134 @@
 use niri_ipc::Action::ScreenshotScreen;
 use sdl3::event::Event;
 use sdl3::image::LoadTexture;
-use sdl3::keyboard::Keycode;
-use std::path::Path;
+use sdl3::render::FRect;
 
-fn main()  {
+const MOVEMENT_SPEED: f32 = 2.0;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tempdir = std::env::temp_dir();
     tempdir.push("whatever");
 
-    let amoguspath = tempdir.as_path().to_str().expect("amogus is sussy").to_string();
+    let amoguspath = tempdir
+        .as_path()
+        .to_str()
+        .expect("amogus is sussy")
+        .to_string();
 
-    let amogus = ScreenshotScreen { write_to_disk: true, show_pointer: false, path: Some(amoguspath) };
+    let amogus = ScreenshotScreen {
+        write_to_disk: true,
+        show_pointer: false,
+        path: Some(amoguspath.clone()),
+    };
 
     let mut socket = niri_ipc::socket::Socket::connect().expect("Failed talking to niri socket");
 
-    socket.send(niri_ipc::Request::Action(amogus)).expect("whatever?").expect("fucking i dont know");
+    socket
+        .send(niri_ipc::Request::Action(amogus))
+        .expect("whatever?")
+        .expect("fucking i dont know");
 
-    sdl_run(tempdir.as_path()).expect("whate the sigma");
-}
-
-
-fn sdl_run(png: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let sdl_context = sdl3::init()?;
+
     let video_subsystem = sdl_context.video()?;
+    let mouse_subsystem = sdl_context.mouse();
+
+    let display_mode = video_subsystem.get_primary_display()?.get_mode()?;
+
     let window = video_subsystem
-        .window("rust-sdl3 demo: Video", 3440, 1440)
+        .window(
+            "Example Renderer Scaling & Moving Textures",
+            display_mode.w as u32,
+            display_mode.h as u32,
+        )
         .position_centered()
-        .fullscreen()
-        .build()
-        .map_err(|e| e.to_string())?;
+        .input_grabbed()
+        .build()?;
+
+    mouse_subsystem.relative_mouse_mode(&window);
+    mouse_subsystem.warp_mouse_in_window(
+        &window,
+        display_mode.w as f32 / 2.0,
+        display_mode.h as f32 / 2.0,
+    );
+    mouse_subsystem.capture(true);
+
+    // x is (x_aspect) times bigger than y
+    let x_aspect: f32 = display_mode.w as f32 / display_mode.h as f32;
 
     let mut canvas = window.into_canvas();
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture(png)?;
+    let texture = texture_creator
+        .load_texture(amoguspath)
+        .expect("Failed loading texture");
 
-    canvas.copy(&texture, None, None)?;
-    canvas.present();
+    let texture_width = texture.width();
+    let texture_height = texture.height();
 
-    'mainloop: loop {
-        for event in sdl_context.event_pump()?.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Option::Some(Keycode::Escape),
-                    ..
-                } => break 'mainloop,
-                _ => {}
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut wheel = 0.0;
+    let mut offset_x_movement = 0.0;
+    let mut offset_y_movement = 0.0;
+    let mut holding_mouse = false;
+
+    loop {
+        for event in event_pump.poll_iter() {
+            if let Event::MouseMotion { xrel, yrel, .. } = event {
+                offset_x_movement -= xrel;
+                offset_y_movement -= yrel;
+            }
+
+            if let Event::MouseWheel { y, .. } = event {
+                if y >= 0.0 {
+                    wheel += 0.05;
+                } else if (wheel - 0.05) > 0.0 {
+                    wheel -= 0.05;
+                } else {
+                    wheel = 0.0;
+                }
+            }
+
+            if let Event::MouseButtonUp { .. } = event {
+                holding_mouse = false;
+            }
+            if let Event::MouseButtonDown { .. } = event {
+                holding_mouse = true;
+            }
+
+            if let Event::Quit { .. } = event {
+                return Ok(());
             }
         }
-    }
 
-    Ok(())
+        // Determine direction and scale
+        let scale = 1.0 + wheel;
+
+        // Calculate horizontal movement
+        // let movement = ((now % 4000) as f32 / 4000.0) * display_mode.w as f32;
+
+        // Clear the canvas
+        canvas.set_draw_color(sdl3::pixels::Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        // Calculate the destination rectangle with scaling and horizontal movement
+        let width = texture_width as f32 * scale;
+        let height = texture_height as f32 * scale;
+
+        // Start at 0, dont move if USER scaling is none
+        // When zooming, make sure that we are zooming towards the center and offsetting by the opposite of the offset that the user has from when they first clicked on the screen so that when they move their cursor the focus goes towards it
+        // Also ensure that it follows the aspect ratio of the screen
+        let x = wheel
+            * (offset_x_movement * MOVEMENT_SPEED
+                - ((texture_width as f32 / 2.0) - offset_x_movement * x_aspect));
+        let y = wheel
+            * (offset_y_movement * MOVEMENT_SPEED
+                - ((texture_height as f32 / 2.0) - offset_y_movement * 1.0));
+
+        let dst_rect = FRect::new(x, y, width, height);
+
+        // Render the texture
+        canvas.copy(&texture, None, Some(dst_rect))?;
+
+        // Present the updated canvas
+        canvas.present();
+    }
 }
