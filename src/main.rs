62832bb9300f05bdbbabeb94c25f163cf1c2b611
Tulip Blossom <tulilirockz@outlook.com>
2025-11-16 03:00:08 -0300

    feat: use ASHPD for screenshotting :)
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,28 @@
-use niri_ipc::Action::ScreenshotScreen;
 use sdl3::event::Event;
 use sdl3::image::LoadTexture;
 use sdl3::render::FRect;
-use std::collections::HashMap;
-use zbus::zvariant::OwnedObjectPath;
-use zbus::zvariant::Value;
-use zbus::{Connection, proxy};
-
-const MOVEMENT_SPEED: f32 = 0.5;
-
-#[proxy(
-    default_service = "org.freedesktop.portal.Desktop",
-    default_path = "/org/freedesktop/portal/desktop",
-    interface = "org.freedesktop.portal.Screenshot"
-)]
-trait ScreenshotPortal {
-    async fn screenshot(
-        &self,
-        parent_window: &str,
-        options: HashMap<String, Value<'static>>,
-    ) -> zbus::Result<OwnedObjectPath>;
-}
+// use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let connection = zbus::Connection::session().await?;
-    // busctl --user call org.freedesktop.portal.Desktop /org/freedesktop/portal/desktop org.freedesktop.portal.Screenshot Screenshot sa{sv} '' 2 handle_token s portal1894239812 interactive b false
-
-    let proxy = ScreenshotPortalProxy::new(&connection).await?;
-    let mut options: HashMap<String, Value<'static>> = HashMap::new();
-    options.insert(
-        "handle_token".to_string(),
-        Value::from("portal1894239812".to_string()),
-    );
-    options.insert("interactive".to_string(), Value::from(false));
-    let return_skibidi = proxy.screenshot("", options).await?;
-    println!("{return_skibidi}");
-
-    // busctl --user wait :1.28 /org/freedesktop/portal/desktop/request/1_715/portal1894239812 org.freedesktop.portal.Request Response
-
-    let mut tempdir = std::env::temp_dir();
-    tempdir.push("whatever");
-
-    let amoguspath = tempdir
+    let amoguspath = ashpd::desktop::screenshot::Screenshot::request()
+        .send()
+        .await
+        .expect("Failed parsing screenshot")
+        .response()
+        .expect("dunno")
+        .uri()
+        .to_file_path()
+        .expect("Failed converting URI to path")
         .as_path()
         .to_str()
         .expect("amogus is sussy")
         .to_string();
 
-    let amogus = ScreenshotScreen {
-        write_to_disk: true,
-        show_pointer: false,
-        path: Some(amoguspath.clone()),
-    };
-
-    let mut socket = niri_ipc::socket::Socket::connect().expect("Failed talking to niri socket");
-
-    socket
-        .send(niri_ipc::Request::Action(amogus))
-        .expect("whatever?")
-        .expect("fucking i dont know");
-
     let sdl_context = sdl3::init()?;
 
-    let video_subsystem = sdl_context.video()?;
     let mouse_subsystem = sdl_context.mouse();
+    let video_subsystem = sdl_context.video()?;
 
     let display_mode = video_subsystem.get_primary_display()?.get_mode()?;
 
@@ -102,7 +60,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut wheel = 0.0;
     let mut offset_x_movement = 0.0;
     let mut offset_y_movement = 0.0;
-    let mut holding_mouse = false;
+    // let mut holding_mouse = false;
 
     loop {
         for event in event_pump.poll_iter() {
@@ -121,12 +79,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
 
-            if let Event::MouseButtonUp { .. } = event {
-                holding_mouse = false;
-            }
-            if let Event::MouseButtonDown { .. } = event {
-                holding_mouse = true;
-            }
+            // if let Event::MouseButtonUp { .. } = event {
+            //     holding_mouse = false;
+            // }
+            // if let Event::MouseButtonDown { .. } = event {
+            //     holding_mouse = true;
+            // }
 
             if let Event::Quit { .. } = event {
                 return Ok(());
@@ -151,11 +109,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // When zooming, make sure that we are zooming towards the center and offsetting by the opposite of the offset that the user has from when they first clicked on the screen so that when they move their cursor the focus goes towards it
         // Also ensure that it follows the aspect ratio of the screen
         let x = wheel
-            * (offset_x_movement * MOVEMENT_SPEED
+            * (offset_x_movement * 0.5
                 - ((texture_width as f32 / 2.0)
                     - (offset_x_movement / (2.0 * x_aspect)) * x_aspect));
         let y = wheel
-            * (offset_y_movement * MOVEMENT_SPEED
+            * (offset_y_movement * 0.5
                 - ((texture_height as f32 / 2.0) - (offset_y_movement / 2.0) * 1.0));
 
         let dst_rect = FRect::new(x, y, width, height);
